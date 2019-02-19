aeef5f6f14b58df56056539685c5f5674583f1d9
Gurkirat Singh <47685401+gurkiratsingh891@users.noreply.github.com>
2019-02-19 23:02:39 +0530

    Add files via upload
    Find GCD of two numbers

---
+++ GCD.java
@@ -0,0 +1,13 @@
+﻿//Find GCD of two numbers
+public class MyClass {
+    static int gcd(int a,int b)
+    {
+        if(b==0)
+        return a;
+        return gcd(b,a%b);
+    }
+    public static void main(String args[]) {
+        
+        System.out.println(gcd(0,10));
+    }
+}
